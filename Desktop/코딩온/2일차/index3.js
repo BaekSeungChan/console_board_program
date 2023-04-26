8743ea58e8d2fc65bb9414172af2e9e75cd477af
baekseungchan1117 <bsc7386@gmail.com>
2023-04-26 14:03:51 +0900

    달력

---
+++ Desktop/코딩온/2일차/index3.js
@@ -0,0 +1,6 @@
+const number = [12, 3, 4, 5, 6];
+
+
+number.forEach(function (number) {
+    console.log(number);
+})
---
+++ Desktop/코딩온/3일차/index3.js
@@ -0,0 +1,8 @@
+function add(x, y) {
+    return x + y;
+}
+
+
+const result = add(2, 3);
+
+console.log(result);
